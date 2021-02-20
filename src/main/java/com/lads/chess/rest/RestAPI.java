4d557edae65752789ca21968268bc53df1f1dd7d
Scott <scott.wares@yahoo.co.uk>
2021-02-20 14:44:53 +0000

    Outlined stub REST endpoints

--- src/main/java/com/lads/chess/rest/RestAPI.java
+++ src/main/java/com/lads/chess/rest/RestAPI.java
@@ -18,13 +18,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class RestAPI {
 	
 	/**
+	 * @implSpec REST endpoint for piece movement
 	 * @apiNote The formal parameter will likely change to POJO mapping
 	 * @param payload: JSON String representing a move
+	 * @return ResponseEntity<String>: HTTP ResponseEntity 
 	 */
 	@PostMapping("/move")
 	@RequestMapping(consumes = "application/json")
 	public ResponseEntity<String>doMove(@RequestBody String payload){
 		return new ResponseEntity<String>("move endpoint", HttpStatus.OK);
+	}
+	
+	/**
+	 * @implSpec REST endpoint for starting a new game
+	 */
+	@PostMapping("/new")
+	@RequestMapping(consumes = "application/json")
+	public ResponseEntity<String>makeNewGame(){
+		return new ResponseEntity<String>("new game endpoint", HttpStatus.OK);
+	}
+	
+	/**
+	 * @implSpec REST endpoint for starting a new game
+	 */
+	@PostMapping("/resign")
+	@RequestMapping(consumes = "application/json")
+	public ResponseEntity<String>resigngame(){
+		return new ResponseEntity<String>("resign endpoint", HttpStatus.OK);
 	}
 
 }
